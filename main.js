c65538e37b133c75446f923bc982e30af3234338
Cole Yeager <106110474+ColeYeager19@users.noreply.github.com>
2023-05-10 22:42:15 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+// JavaScript Document
+/*eslint-env es6*/
+$(function () {
+
+
+        });
+				
+
+function toggleFirst() {
+	$("div42").toggle();
+	$("div43").toggle();
+}
+
+$(document).ready(function () {
+	$("div43").toggle();
+	setInterval(toggleFirst, 20000)
+});
+
+const full = document.querySelector(".full");
+const previews = document.querySelectorAll(".gallery img");
+const original = document.querySelector(".full-img");
+
+previews.forEach((preview) => {
+	preview.addEventListener("click",() => {
+		full.classList.add("open");
+		original.classList.add("open");
+		const originalSrc = preview.getAttribute("data-original");
+		original.src = `./media/full/${originalSrc}`;
+	});
+});
+
+full.addEventListener("click", (e) => {
+	if (e.target.classList.contains("full")) {
+		full.classList.remove("open");
+				original.classList.remove("open");
+	}
+});
