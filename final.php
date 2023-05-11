aed6352fa4ef00eef130ebca2abbb94a537c3fc2
Cole Yeager <106110474+ColeYeager19@users.noreply.github.com>
2023-05-10 23:50:57 -0600

    Update final.php

--- final.php
+++ final.php
@@ -1,10 +1,17 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
+    <meta charset="UTF-8">
+    <title>Contact Form</title>
 </head>
-
 <body>
+    <h1>Thank You</h1>
+    <p>Here is the information you have submitted:</p>
+    <ol>
+        <li><em>First Name:</em> <?php echo $_POST["fname"]?></li>
+        <li><em>Last Name:</em> <?php echo $_POST["lname"]?></li>
+        <li><em>Country:</em> <?php echo $_POST["country"]?></li>
+        <li><em>Subject:</em> <?php echo $_POST["subject"]?></li>
+    </ol>
 </body>
 </html>
